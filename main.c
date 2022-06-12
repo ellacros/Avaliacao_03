a67e194d5c18e283cce1613844b2911861e7655f
ellacros <brunellacouto@ucl.br>
2022-06-12 18:53:00 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "MinhaBiblioteca.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+
+  tletras frase_cod;
+  char frase[150];
+  printf("Escreva uma frase ");
+  fgets(frase, 150, stdin);
+  scanf("%c", frase);
+  
+  tletras trocarletras(char x);
+  char letrasubstituida(char y);
+  
+  return 0;
+}
