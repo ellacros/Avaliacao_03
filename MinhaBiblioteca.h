e6fbe32a3df81ae74392db0f816f43e79d6f3bcd
ellacros <brunellacouto@ucl.br>
2022-06-12 18:53:07 -0300

    Create MinhaBiblioteca.h

---
+++ MinhaBiblioteca.h
@@ -0,0 +1,15 @@
+#ifndef __MinhaBiclioteca_H
+#define __MinhaBiblioteca_H
+
+typedef struct frase_codif
+{
+
+  char letrasubstituida;
+  char letratrocada;
+
+}tletras;
+
+tletras trocarletras(char x);
+char letrasubstituida(char y);
+
+#endif
